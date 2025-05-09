2a0668e6ba796a32e0d6a995734bc6c00de4c196
Jacob Loveland <Jacob_W_Loveland@yahoo.com>
2025-05-09 08:21:40 -0600

    W02 Prove: Developer—C# Programming Parts 3–5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("I've picked a number between 1 and 100. Try to guess it!");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
